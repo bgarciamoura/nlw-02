3deab641d22cdb3d52c99db869dee9b2958d23fd
bgarciamoura <bgarciamoura@gmail.com>
2020-08-08 15:37:13 -0300

    Landing page created

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import logo from '../../assets/images/logo.svg';
+import landing from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import "./style.css";
+
+const Landing = () => {
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logo} alt="Proffy Logo" />
+                    <h2>Sua plataforma de estudos online</h2>
+                </div>
+                <img
+                    src={landing}
+                    alt="Plataforma de estudos"
+                    className="hero-image"
+                />
+
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar" />
+                        Estudar
+                    </a>
+
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar Aulas" />
+                        Dar Aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas
+                    <img src={purpleHeartIcon} alt="Coração roxo" />
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
